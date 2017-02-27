d999f1ca772d299c38bd84088994cc71790dab74
Kyle Joyce <kyyjoyce@gmail.com>
2017-02-27 13:27:05 -0700

    applying styles to header with flexbox

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,34 @@
 // Import a library to create a Component
 import React from 'react';
 // import destructuring used to import only what is needed from react-native
-import { Text } from 'react-native';
+// these are primitive elements available in react-native
+import { Text, View } from 'react-native';
 
 // Create a Component - Objects that create text or content to render
 // Components use JSX, which looks like html but compiles to javascript
 const Header = () => {
-  return (
-    <Text>Header Text</Text>
-  );
+    const { textStyle, viewStyle } = styles;
+
+    return (
+      <View style={viewStyle}>
+        <Text style={textStyle}>Header Text</Text>
+      </View>
+    );
 };
 
 const styles = {
+
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15
+  },
   textStyle: {
     fontSize: 20
   }
+
 };
 
 // Export component to import in App
