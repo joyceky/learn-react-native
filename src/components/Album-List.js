490eaf13e2c6ba348bf299d9897970636e9536d8
Kyle Joyce <kyyjoyce@gmail.com>
2017-02-27 14:24:52 -0700

    album list convert from functional component to class-based

--- src/components/Album-List.js
+++ src/components/Album-List.js
@@ -1,32 +1,21 @@
 // Import a library to create a Component
-import React from 'react';
+import React, { Component } from 'react';
 // import destructuring used to import only what is needed from react-native
 // these are primitive elements available in react-native
 import { Text, View } from 'react-native';
 
 // Create a Component - Objects that create text or content to render
 // Components use JSX, which looks like html but compiles to javascript
-const AlbumList = () => {
-    const { textStyle, viewStyle } = styles;
-
+class AlbumList extends Component {
+  render() {
     return (
-      <View style={viewStyle}>
-        <Text style={textStyle}>Album List</Text>
+      <View>
+        <Text>Album List</Text>
       </View>
     );
-};
-
-const styles = {
-  viewStyle: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 20
-  },
-  textStyle: {
-    fontSize: 20
   }
+}
 
-};
 
 // Export component to import in App
 export default AlbumList;
