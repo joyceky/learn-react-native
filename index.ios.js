54e884afdea322944283aa16627206122ad50c92
Kyle Joyce <kyyjoyce@gmail.com>
2017-02-27 14:17:33 -0700

    album list component connected

--- index.ios.js
+++ index.ios.js
@@ -7,20 +7,28 @@ and makes them work together
 
 REACT-NATIVE provides basic mobile components and renders
 them on the mobile screen
+
+http://rallycoding.herokuapp.com/api/music_albums
 */
 
 // Import a library to create a Component
 import React from 'react';
 // import destructuring used to import only what is needed from react-native
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 // import our components to nest inside App and render
-import Header from './src/components/header';
+import Header from './src/components/Header';
+import AlbumList from './src/components/Album-List';
 
 // Create a Component - Objects that create text or content to render
 // Components use JSX, which looks like html but compiles to javascript
-const App = () => (
+const App = () => {
+return (
+  <View>
     <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
   );
+};
 
 // Render it to the device with ReactNative
 AppRegistry.registerComponent('albums', () => App);
